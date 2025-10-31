758400238f3ed034bedaa95682452d9b9eaf5c6b
zzunho9 <jamlove4763@naver.com>
2025-10-31 18:11:01 +0900

    feat: 보너스 번호 입력 기능 추가

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -2,7 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoView {
@@ -34,17 +34,35 @@ public class LottoView {
     }
 
     private Lotto parseLotto(String[] inputValues) {
+        validateLotto(inputValues);
+        List<Integer> numbers = Arrays.stream(inputValues).map(Integer::parseInt).toList();
+        return new Lotto(numbers);
+    }
+
+    private void validateLotto(String[] inputValues) {
         if (inputValues.length != 6)
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        List<Integer> numbers = new ArrayList<>();
+        int[] numbers = new int[46];
         for (String inputValue : inputValues) {
             int number = parseInteger(inputValue);
             if (number < 1 || number > 45)
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
-            numbers.add(number);
+            if (1 < numbers[number])
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+            numbers[number]++;
         }
-        if (numbers.stream().distinct().count() != numbers.size())
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-        return new Lotto(numbers);
+    }
+
+    public int inputBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String inputValue = Console.readLine().trim();
+        int bonusNumber = parseInteger(inputValue);
+        validateBonusNumber(bonusNumber);
+        return bonusNumber;
+    }
+
+    public void validateBonusNumber(int number) {
+        if (number < 1 || number > 45)
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
     }
 }
