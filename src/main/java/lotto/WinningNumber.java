5fb05621aaaae92521b5f7f865fd64b6f5b06c3d
zzunho9 <jamlove4763@naver.com>
2025-11-01 23:59:12 +0900

    test: 로또 도메인 및 당첨 로직 단위 테스트 추가

--- src/main/java/lotto/WinningNumber.java
+++ src/main/java/lotto/WinningNumber.java
@@ -5,14 +5,17 @@ public class WinningNumber {
     private final int bonusNumber;
 
     public WinningNumber(Lotto winningLotto, int bonusNumber) {
-        validate(bonusNumber);
+        validate(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
-    private void validate(int bonusNumber) {
-        if (bonusNumber < 1 || bonusNumber > 45)
+    private void validate(Lotto winningLotto, int bonusNumber) {
+        if(bonusNumber < 1 || bonusNumber > 45)
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
+
+        if(winningLotto.getNumbers().contains(bonusNumber))
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 다른 번호와 중복될 수 없습니다.");
     }
 
     public Lotto getWinningLotto() {
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
+    private LottoService lottoService = new LottoService();
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -21,5 +24,38 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
-}
+    @DisplayName("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외가 발생한다.")
+    @Test
+    void 당첨번호와_보너스번호가_중복되면_예외() {
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThatThrownBy(() -> new WinningNumber(winningLotto, 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("5등 1개에 해당할 경우 수익률은 정확히 62.5%이다.")
+    @Test
+    void 수익률_계산() {
+        Map<LottoRank, Integer> result = Map.of(
+                LottoRank.FIRST, 0,
+                LottoRank.SECOND, 0,
+                LottoRank.THIRD, 0,
+                LottoRank.FOURTH, 0,
+                LottoRank.FIFTH, 1,
+                LottoRank.MISS, 0
+        );
+        int purchaseAmount = 8000;
+
+        double profitRate = lottoService.calculateProfitRate(result, purchaseAmount);
+
+        assertThat(profitRate).isEqualTo(62.5);
+    }}
