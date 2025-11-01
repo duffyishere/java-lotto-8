25747eb122b3f4feb36d11d5fd608670e845a90a
zzunho9 <jamlove4763@naver.com>
2025-11-01 21:49:14 +0900

    fix: 보너스 번호를 검증하는 로직의 위치 변경

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -30,39 +30,13 @@ public class LottoView {
     public Lotto inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String[] inputValues = Console.readLine().trim().split(",");
-        return parseLotto(inputValues);
-    }
-
-    private Lotto parseLotto(String[] inputValues) {
-        validateLotto(inputValues);
         List<Integer> numbers = Arrays.stream(inputValues).map(Integer::parseInt).toList();
         return new Lotto(numbers);
-    }
-
-    private void validateLotto(String[] inputValues) {
-        if (inputValues.length != 6)
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        int[] numbers = new int[46];
-        for (String inputValue : inputValues) {
-            int number = parseInteger(inputValue);
-            if (number < 1 || number > 45)
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
-            if (1 < numbers[number])
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-            numbers[number]++;
-        }
     }
 
     public int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         String inputValue = Console.readLine().trim();
-        int bonusNumber = parseInteger(inputValue);
-        validateBonusNumber(bonusNumber);
-        return bonusNumber;
-    }
-
-    public void validateBonusNumber(int number) {
-        if (number < 1 || number > 45)
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
+        return parseInteger(inputValue);
     }
 }
---
+++ src/main/java/lotto/WinningNumber.java
@@ -0,0 +1,23 @@
+package lotto;
+
+public class WinningNumber {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningNumber(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45)
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이여야 합니다.");
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
